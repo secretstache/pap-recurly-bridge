bd5697c6b87ffa8ebf28e438f63c30231a41e3e2
Aman Deep <aman@pradent.com>
2017-01-09 22:33:38 +0530

    Added dependency check to make sure postaffiliatepro is active

--- pap-recurly-bridge.php
+++ pap-recurly-bridge.php
@@ -52,3 +52,22 @@ function prb_options_page() {
     );
 }
 add_action('admin_menu', 'prb_options_page');
+
+// Check if PAP active
+function prb_check_if_pap_active() {
+  if ( is_admin() && current_user_can( 'activate_plugins' ) &&  !is_plugin_active( 'postaffiliatepro/postaffiliatepro.php' ) ) {
+    add_action( 'admin_notices', 'prb_check_if_pap_active_notice' );
+
+    deactivate_plugins( plugin_basename( __FILE__ ) );
+
+    if ( isset( $_GET['activate'] ) ) {
+      unset( $_GET['activate'] );
+    }
+  }
+}
+add_action( 'admin_init', 'prb_check_if_pap_active' );
+
+// Dependency missing notice
+function prb_check_if_pap_active_notice(){
+  ?><div class="error"><p>PAP Recurly Bridge has been deactivated as requires the plugin 'postaffiliatepro' to be installed and activated.</p></div><?php
+}
